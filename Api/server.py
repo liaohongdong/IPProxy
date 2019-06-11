4a839c199412b0b8275eaf9ea1274dd20a4688c9
liaohongdong <liaodoho@gmail.com>
2019-06-11 16:21:59 +0800

    test

---
+++ Api/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return '123'
+
+
+def run():
+    app.run(host='0.0.0.0', port=5000)
+
+
+if __name__ == '__main__':
+    run()
--- Util/LogHandler.py
+++ Util/LogHandler.py
@@ -18,7 +18,14 @@ CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 ROOT_PATH = os.path.join(CURRENT_PATH, os.pardir)
 LOG_PATH = os.path.join(ROOT_PATH, 'log')
 
+
+class LogHandler(logging.Logger):
+    pass
+
+
 if __name__ == "__main__":
-    #Print.out(os.path.abspath(__file__))
-    #Print.out(os.path.dirname(os.path.abspath(__file__)))
-    Print.out(os.pardir)
+    # Print.out(os.path.abspath(__file__))
+    # Print.out(os.path.dirname(os.path.abspath(__file__)))
+    Print.out(LOG_PATH)
+    Print.out(CURRENT_PATH)
+    Print.out(ROOT_PATH)
