249f4aa34a537e6c816e841a830f11944a5a0d3b
liaohongdong <liaodoho163@qq.com>
2019-06-11 16:15:50 +0800

    test

---
+++ Config/setting.py
@@ -0,0 +1,33 @@
+DATABASES = {
+    "default": {
+        "TYPE": "redis",
+        "HOST": "39.108.115.177",
+        "PORT": 6379,
+        "NAME": "proxy",
+        "PASSWORD": ""
+    }
+}
+
+PROXY_GETTER = [
+    "freeProxyFirst",
+    "freeProxySecond",
+    # "freeProxyThird",  # 网站已不能访问
+    "freeProxyFourth",
+    "freeProxyFifth",
+    # "freeProxySixth"   # 不再提供免费代理
+    "freeProxySeventh",
+    # "freeProxyEight",
+    # "freeProxyNinth",
+    "freeProxyTen",
+    "freeProxyEleven",
+    "freeProxyTwelve",
+    # foreign website, outside the wall
+    # "freeProxyWallFirst",
+    # "freeProxyWallSecond",
+    # "freeProxyWallThird"
+]
+
+SERVER_API = {
+    "HOST": "0.0.0.0",
+    "PORT": 5000
+}
