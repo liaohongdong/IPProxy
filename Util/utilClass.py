56808b6c6263bf0f874c390e7b9f1e688f2b9d63
liaohongdong <liaodoho@gmail.com>
2019-06-13 18:18:16 +0800

    test

--- Util/utilClass.py
+++ Util/utilClass.py
@@ -2,8 +2,31 @@ import os
 
 
 class LazyProperty(object):
-    pass
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        else:
+            value = self.func(instance)
+            setattr(instance, self.func.__name__, value)
+            return value
 
 
 class Singleton(type):
+    _inst = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._inst:
+            cls._inst[cls] = super(Singleton, cls).__call__(*args)
+        return cls._inst[cls]
+
+
+class Liao(object):
+    def __init__(self):
+        print('asdf')
+
+
+if __name__ == '__main__':
     pass
---
+++ Util/utilFunction.py
@@ -0,0 +1,10 @@
+import requests
+import time
+from lxml import etree
+
+from Util.LogHandler import Loghandler
+from Util.WebRequest import WebRequest
+
+
+if __name__ == '__main__':
+    pass
