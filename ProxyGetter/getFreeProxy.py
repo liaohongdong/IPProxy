006bfbbee162f93586f669b1012803eb106344b1
liaohongdong <liaodoho@gmail.com>
2019-06-18 10:49:30 +0800

    test

--- ProxyGetter/getFreeProxy.py
+++ ProxyGetter/getFreeProxy.py
@@ -2,6 +2,8 @@ import re
 import sys
 import requests
 import time
+import json
+
 from Util.Print import o
 from Util.utilFunction import getHtmlTree
 from Util.WebRequest import WebRequest
@@ -28,7 +30,19 @@ class GetFreeProxy(object):
             ul_list = html_tree.xpath('//ul[@class="l2"]')
             for ul in ul_list:
                 try:
-                    yield ':'.join(ul.xpath('.//li/text()')[0:2])
+                    arr = ul.xpath('.//li/text()')
+                    yield {
+                        'ip': arr[0],  # 地址
+                        'port': arr[1],  # 端口
+                        'anonymity': arr[2],  # 透明度
+                        'type': arr[3],  # 网络类型
+                        'country': arr[4],  # 国家
+                        'province': arr[5],  # 省市
+                        'operator': arr[6],  # 运营商
+                        'resp_speed': arr[7],  # 响应速度
+                        'final_verify_time': arr[8],  # 最后验证时间
+                        'life': '',  # 存活时间
+                    }
                 except Exception as e:
                     print(e)
 
@@ -40,23 +54,97 @@ class GetFreeProxy(object):
         ]
         headers = {
             'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
-            'Cookie': '__jsluid=791a40e3ae570ed6fcbe6ae547617390; __jsl_clearance=1560760725.943|0|LZc2w%2FdLv49feXKKfnYUm4SIy1s%3D'
         }
         for index, _ in enumerate(urls):
             url = _.format(count=count)
-            cls.driver(url, headers)
+            page = cls.driver(url, headers)
+            ips = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}", page)
+            for item in ips:
+                arr = item.split(':')
+                yield {
+                    'ip': arr[0],  # 地址
+                    'port': arr[1],  # 端口
+                    'anonymity': '',  # 透明度
+                    'type': '',  # 网络类型
+                    'country': '',  # 国家
+                    'province': '',  # 省市
+                    'operator': '',  # 运营商
+                    'resp_speed': '',  # 响应速度
+                    'final_verify_time': '',  # 最后验证时间
+                    'life': '',  # 存活时间
+                }
 
-            # html = request.get(url).content
-            # ips = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}", html.decode('utf-8'))
-            # o(ips)
-            # for ip in ips:
-            #     ip = ip.decode('utf-8')
-            #     print(ip.strip())
-            #     yield ip.strip()
+    @staticmethod  # 有点用不了 网站打开很慢
+    def freeProxyThird():
+        url = 'http://www.ip181.com/'
+        # html_tree = getHtmlTree(url)
+        resp = requests.get(url, timeout=30)
+        html_tree = resp.content
+        try:
+            jsonstr = json.loads(html_tree.decode('utf-8'))
+            result = jsonstr['RESULT']
+            for i in result:
+                yield {
+                    'ip': i['ip'],  # 地址
+                    'port': i['port'],  # 端口
+                    'anonymity': '',  # 透明度
+                    'type': '',  # 网络类型
+                    'country': '',  # 国家
+                    'province': i['position'],  # 省市
+                    'operator': '',  # 运营商
+                    'resp_speed': '',  # 响应速度
+                    'final_verify_time': '',  # 最后验证时间
+                    'life': '',  # 存活时间
+                }
+        except Exception as e:
+            pass
+
+    @staticmethod
+    def freeProxyFourth(page_count=1):
+        url_list = [
+            'http://www.xicidaili.com/nn/',  # 高匿
+            'http://www.xicidaili.com/nt/',  # 透明
+            'https://www.xicidaili.com/wn/',  # https
+            'https://www.xicidaili.com/wt/',  # http
+        ]
+        for each_url in url_list:
+            for i in range(1, page_count + 1):
+                url = each_url + str(i)
+                tree = getHtmlTree(url)
+                proxy_list = tree.xpath('.//table[@id="ip_list"]//tr[position()>1]')
+                for proxy in proxy_list:
+                    try:
+                        ip = proxy.xpath('./td[2]/text()')
+                        port = proxy.xpath('./td[3]/text()')
+                        province = proxy.xpath('./td[4]/a/text()')
+                        anonymity = proxy.xpath('./td[5]/text()')
+                        type = proxy.xpath('./td[6]/text()')
+                        resp_speed = proxy.xpath('./td[7]/div[1]/@title')
+                        life = proxy.xpath('./td[9]/text()')
+                        final_verify_time = proxy.xpath('./td[10]/text()')
+                        # o(ip+port+province+anonymity+type+resp_speed+left+final_verify_time)
+                        yield {
+                            'ip': ip[0],  # 地址
+                            'port': port[0],  # 端口
+                            'anonymity': anonymity[0],  # 透明度
+                            'type': type[0],  # 网络类型
+                            'country': '',  # 国家
+                            'province': province[0],  # 省市
+                            'operator': '',  # 运营商
+                            'resp_speed': resp_speed[0],  # 响应速度
+                            'final_verify_time': final_verify_time[0],  # 最后验证时间
+                            'life': life[0],  # 存活时间
+                        }
+                    except Exception as e:
+                        o(e)
+
+    @staticmethod
+    def freeProxyFifth():
+        url = "http://www.goubanjia.com/"
 
     @staticmethod
     def driver(url, headers):
-        print(url)
+        # print(url)
         options = Options()
         options.add_argument('--headless')
         options.add_argument('user-agent=' + headers['User-Agent'])
@@ -65,16 +153,19 @@ class GetFreeProxy(object):
         driver.get(url)
         cookies_clearance = driver.get_cookie('__jsl_clearance')
         cookies_jsluid = driver.get_cookie('__jsluid')
+        # o(cookies_clearance)
+        # o(cookies_jsluid)
         driver.delete_all_cookies()
         driver.add_cookie({'name': '__jsl_clearance', 'value': cookies_clearance['value']})
         driver.add_cookie({'name': '__jsluid', 'value': cookies_jsluid['value']})
-
         driver.get(url)
         driver.implicitly_wait(1)
         driver.maximize_window()
         time.sleep(1)
         html = driver.page_source
-        open(str(time.gmtime().tm_sec) + 'a.html', 'w').write(html)
+        return html
+        # o(html)
+        # open(str(time.gmtime().tm_sec) + 'a.html', 'w').write(html)
         # -------------------------------------- #
         # d.get('http://www.baidu.com')
         # d.implicitly_wait(1)
@@ -96,9 +187,9 @@ class GetFreeProxy(object):
 if __name__ == '__main__':
     g = GetFreeProxy()
     # d = g.freeProxyFirst()
-    # g.freeProxySecond()
-    # g.driver()
-    g.freeProxySecond()
-
-    # for i in d:
-    #     o(i)
+    # d = g.freeProxySecond()
+    # d = g.freeProxyThird()
+    # d = g.freeProxyFourth()
+    d = g.freeProxyFifth()
+    for i in d:
+        o(i)
--- Util/WebRequest.py
+++ Util/WebRequest.py
@@ -9,16 +9,29 @@ class WebRequest(object):
         pass
 
     @property
+    def cookies(self):
+        return requests.session()
+
+    @property
     def user_agent(self):
         ua_list = [
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71',
-            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)',
-            'Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0',
+            # 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101',
+            # 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122',
+            # 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71',
+            # 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95',
+            # 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71',
+            # 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)',
+            # 'Mozilla/5.0 (Windows NT 5.1; U; en; rv:1.8.1) Gecko/20061208 Firefox/2.0.0 Opera 9.50',
+            # 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+            'Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
+            'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5',
+            'Mozilla/5.0 (Linux; U; Android 2.2.1; zh-cn; HTC_Wildfire_A3333 Build/FRG83D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+            'MQQBrowser/26 Mozilla/5.0 (linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+            'Opera/9.80 (Android 2.3.4; Linux; Opera Mobi/build-1107180945; U; en-GB) Presto/2.8.149 Version/11.10',
+            'Mozilla/5.0 (BlackBerry; U; BlackBerry 9800; en) AppleWebKit/534.1+ (KHTML, like Gecko) Version/6.0.0.337 Mobile Safari/534.1+',
+            'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+            'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50'
         ]
         return random.choice(ua_list)
 
